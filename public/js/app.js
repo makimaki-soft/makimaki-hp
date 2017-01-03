535c60742234f72c52d97c388bed9cea894c08ce
sryoji <sryoji5087@gmail.com>
2017-01-04 01:28:40 +0900

    スマホ対応

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
-angular.module('myApp', []);
+var app = angular.module('hpApp',[]);
+// ---------------------------------------------------------
+// 右上のメニューの表示
+// ---------------------------------------------------------
+app.controller('headerController',function($scope){
+
+    $scope.onclick = function() {
+        console.log("test");
+        $("header ul.nav").toggle();
+        $("header div.logosp p").toggle();
+    }
+});
